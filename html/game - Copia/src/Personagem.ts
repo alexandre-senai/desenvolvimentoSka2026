d62abca0827367df2121ef00e9ff26ec056d15da
alexandre <alexandre.l@edu.sc.senai.br>
2026-05-12 20:04:20 -0300

    adicionando jogo novamente

---
+++ html/game - Copia/src/Personagem.ts
@@ -0,0 +1,70 @@
+export abstract class Personagem {
+  public nome: string = "personagem";
+  protected forca: number = 0;
+  protected vida: number = 0;
+  protected cura: number = 0;
+  protected defesa:number = 0;
+  protected imagem:string = "";
+
+  private jaUsouCura:boolean = false;
+
+  constructor(nome: string, forca: number, vida: number, cura:number, defesa:number, 
+    imagem:string) {
+    this.nome = nome;
+    this.forca = forca;
+    this.vida = vida;
+    this.cura = cura;
+    this.defesa = defesa;
+    this.imagem = imagem;
+  }
+
+  isContinuaVivo(): boolean {
+    return this.vida > 0;
+  }
+
+  sofrerAtaque(dano: number): void {
+    let danoReal = dano - ( dano * (this.defesa / 100) );
+
+    this.vida = this.vida - danoReal;
+
+    this.log(`${this.nome} recebeu ${danoReal} de dano. vida atual: ${this.vida}`)
+
+    this.usarCura();
+  }
+
+  getVida(){
+    return this.vida;
+  }
+
+  getImg(){
+    return this.imagem;
+  }
+
+  public log(mensagem: string) {
+    console.log(mensagem);
+
+    
+    document.getElementById("console")!.innerHTML += '<p>' + mensagem + '</p>';
+  }
+
+
+  usarCura(): void {
+    if (this.vida <= 50 && !this.jaUsouCura) {
+      this.vida = this.vida + this.cura;
+      this.jaUsouCura = true;
+      this.log(
+      `${this.nome} recebeu ${this.cura} de cura. vida atual: ${this.vida}`,
+    );
+    }
+  }
+
+  gerarAtaque(): number {
+    let maximoAtk = 3;
+    return Math.floor(Math.random() * maximoAtk);
+  }
+
+  public abstract atacar(persona: Personagem): void;
+}
+
+// ATIVIDADE CRIAR DOIS PERSONAGEM QUE EXTENDE DA CLASSE PERSONAGEM
+// CRIAR UM CONSTRUTOR E O METODO ATACAR
---
+++ html/game - Copia/src/jogoIndex.ts
@@ -0,0 +1,10 @@
+
+import { Guerreiro } from "./Guerreiro.ts";
+import { Jogo } from "./Jogo.ts";
+import { Mago } from "./Mago.ts";
+
+let mago:Mago = new Mago("Mago", 120, 200);
+let guerreiro:Guerreiro= new Guerreiro("Guerreiro", 100, 300);
+
+let jogo:Jogo = new Jogo();
+jogo.inicia(mago, guerreiro);
